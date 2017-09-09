dad81a706004ffd85ea0e2527ac4f166bd8223e2
ckudlack <thebigcheese40@gmail.com>
2017-09-08 17:03:16 -0700

    moved mainActivity

--- app/src/main/java/com/cdk/skeletonproject/ui/MainActivity.java
+++ app/src/main/java/com/cdk/skeletonproject/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.cdk.skeletonproject;
+package com.cdk.skeletonproject.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.cdk.skeletonproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
