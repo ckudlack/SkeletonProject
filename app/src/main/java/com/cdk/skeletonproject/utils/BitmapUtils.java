5d4a0637c91f676cd1588a4749d9153f099c0c43
ckudlack <thebigcheese40@gmail.com>
2017-09-15 15:11:40 -0700

    Added bitmap utils

---
+++ app/src/main/java/com/cdk/skeletonproject/utils/BitmapUtils.java
@@ -0,0 +1,25 @@
+package com.cdk.skeletonproject.utils;
+
+import android.graphics.Color;
+
+public class BitmapUtils {
+
+    public static int calculateAverageColor(android.graphics.Bitmap bitmap, int pixelSpacing) {
+        int R = 0;
+        int G = 0;
+        int B = 0;
+        int height = bitmap.getHeight();
+        int width = bitmap.getWidth();
+        int n = 0;
+        int[] pixels = new int[width * height];
+        bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+        for (int i = 0; i < pixels.length; i += pixelSpacing) {
+            int color = pixels[i];
+            R += Color.red(color);
+            G += Color.green(color);
+            B += Color.blue(color);
+            n++;
+        }
+        return Color.rgb(R / n, G / n, B / n);
+    }
+}
